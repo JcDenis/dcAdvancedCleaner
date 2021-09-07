58ed7adfb315ba9ae9044dbe2401292e98559a5d
Jean-Christian Denis <contact@jcdenis.fr>
2021-09-07 11:06:03 +0200

    dev branch

--- _define.php
+++ _define.php
@@ -19,7 +19,7 @@ $this->registerModule(
     'Advanced cleaner',
     'Make a huge cleaning of dotclear',
     'Jean-Christian Denis and Contributors',
-    '0.8',
+    '0.8.1',
     [
         'requires' => [['core', '2.19']],
         'permissions' => null,
