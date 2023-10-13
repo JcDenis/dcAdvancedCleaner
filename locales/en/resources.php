0d7b97346a8ef692ec328ff15ceb34495dea7cce
Jean-Christian Denis <contact@jcdenis.fr>
2023-10-13 21:24:38 +0200

    cosmetic

--- locales/en/resources.php
+++ locales/en/resources.php
@@ -8,5 +8,4 @@
  * @author      Jean-Christian Denis (author)
  * @copyright   GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
 \Dotclear\App::backend()->resources()->set('help', 'dcAdvancedCleaner', __DIR__ . '/help/dcAdvancedCleaner.html');
